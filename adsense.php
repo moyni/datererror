8a78bc0f69e3a313c662d4d23c818b923fb14aba
moyni <maria@gocardless.com>
2015-12-01 14:22:22 +0000

    adsense again

---
+++ adsense.php
@@ -0,0 +1,15 @@
+<?php
+$googleadsensecode = '
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+      <!-- bottom ads -->
+      <ins class="adsbygoogle"
+           style="display:block"
+           data-ad-client="ca-pub-7218753973510881"
+           data-ad-slot="6236499056"
+           data-ad-format="auto"></ins>
+      <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>';
+
+echo $googleadsensecode;
+?>
